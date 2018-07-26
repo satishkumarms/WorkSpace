3dd4f935e78c9b8c5d3823b3e40fac9a9b3dc8d0
satishkumarms <smusleshankar@manh.com>
2018-07-26 13:32:48 +0530

    hibernatespringdemo

---
+++ HibernateSpringDemo/src/main/java/com/controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+	
+	@RequestMapping("/")
+	
+	public String test()
+	{
+		return "test";
+	}
+}
